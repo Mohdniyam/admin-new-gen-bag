7079ac4daaf9dce90141cc3e2b7b42b716f34b0f
Mohdniyam <niyamul.haque98@gmail.com>
2025-12-13 16:37:17 +0530

    admin-panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 export function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="font-medium">Hello World</div>
+return (
+    <div>
+        <h1>hello world</h1>
     </div>
-  )
+);
 }
 
-export default App
+export default App;
